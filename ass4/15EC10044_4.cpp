0df1ad50c464a548dd4645dc69d61165c12d4a61
rajeswarim81 <rajeswarim81@gmail.com>
2018-09-14 17:22:36 +0530

    change in datasets

--- ass4/15EC10044_4.cpp
+++ ass4/15EC10044_4.cpp
@@ -19,7 +19,7 @@ bool compdist(const vector<float>& v1, const vector<float>& v2){
 
 void KNNClassifier(vector< vector<float> > tab, vector<float> target){
 	ifstream ifile;               //read the test file
-	ifile.open("test4.csv");
+	ifile.open("test.csv");
 
 	ofstream ofile;               //ouput file
 	ofile.open("15EC10044_4.out");
@@ -29,10 +29,10 @@ void KNNClassifier(vector< vector<float> > tab, vector<float> target){
 		return ;
 	}
 
-	int rows=4,columns=8;
+	int rows=26,columns=4;
 	vector< vector<float> > tabvtest;
     string tabtest[rows][columns];
-    int x;
+    float x;
 
 	for(i=0; i<rows; i++){           //read the test file onto a 2d vector
 		vector<float> dummytest;
@@ -41,16 +41,18 @@ void KNNClassifier(vector< vector<float> > tab, vector<float> target){
 			stringstream s(tabtest[i][j]);
 			s>>x;
 			dummytest.push_back(x);
+			//cout<<x<<"\t";
 		}
 		ifile>>x;
 		dummytest.push_back(x);
+		//cout<<x<<"\n";
         tabvtest.push_back(dummytest);
 	}
 
 	
 
 	float distance;
-	int testrows=4, testcolumns=8, trainrows=20, traincolumns=8;
+	int testrows=26, testcolumns=4, trainrows=74, traincolumns=4;
 	int count;
 
 	for(i=0;i<testrows;i++){                  //read each row of the test file
@@ -58,7 +60,7 @@ void KNNClassifier(vector< vector<float> > tab, vector<float> target){
         distance=0,count=0;
 		
         for(j=0;j<trainrows;j++){             //compute distances
-             
+             distance=0;
              vector<float> dummy;
              for(k=0;k<traincolumns;k++)
              	distance+= ((tabvtest[i][k]-tab[j][k]) * (tabvtest[i][k]-tab[j][k]));
@@ -66,9 +68,16 @@ void KNNClassifier(vector< vector<float> > tab, vector<float> target){
              dummy.push_back(sqrt(distance));
              kdistance.push_back(dummy);
              }
-            
-             sort(kdistance.begin(), kdistance.end(), compdist);  //sort the distances
+            /* if(i==0)
+             for(j=0;j<trainrows;j++)
+             	cout<<kdistance[j][0]<<"\t"<<kdistance[j][1]<<"\n";
+*/
+            sort(kdistance.begin(), kdistance.end(), compdist);  //sort the distances
              
+           /*  if(i==2)
+             for(j=0;j<trainrows;j++)
+             	cout<<kdistance[j][0]<<"\t"<<kdistance[j][1]<<"\n";
+           */  
              for(k=0;k<5;k++){                       //choose majority of the nearest 5 neighbours
              	if(target[(int)kdistance[k][0]])
              		count++;
@@ -80,7 +89,7 @@ void KNNClassifier(vector< vector<float> > tab, vector<float> target){
              }
              	
              else{
-             //	cout<<0;
+               // cout<<0;
              	ofile<<0<<"\t";
              }
              	
@@ -95,20 +104,20 @@ void KNNClassifier(vector< vector<float> > tab, vector<float> target){
 
 main(){
 	ifstream ifile;
-	ifile.open("data4.csv"); //read the input file
+	ifile.open("train.csv"); //read the input file
 
 	if(!ifile.is_open()){
 		cout<<"File couldn't be read!";
 		return 0;
 	}
 
-	const int rows=20, columns=8;
+	const int rows=74, columns=4;
 
 	string tab[rows][columns], target[rows];
     vector< vector<float> > tabv;
     vector<float> targetv; 
 
-    int x,y,k;
+    float x,y,k;
 
 	for(i=0; i<rows; i++){        //read the input file onto a string array and then convert to a float 2d vector
 		vector<float> dummy;
@@ -117,6 +126,7 @@ main(){
 			stringstream s(tab[i][j]);
 			s>>x;
 			dummy.push_back(x);
+			//cout<<x<<"\t";
 		
 		}
         tabv.push_back(dummy);
@@ -125,6 +135,7 @@ main(){
 		stringstream d(target[i]);
 		d>>y;
 		targetv.push_back(y);
+		//cout<<y<<"\n";
 		
 	}
 
--- ass4/practice.py
+++ ass4/practice.py
@@ -3,11 +3,11 @@ import pandas as pd
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
 
-df=pd.read_csv("data4.csv", header=None)
-x=df[df.columns[0:8]]
-y=df[df.columns[8:9]]
-df_=pd.read_csv("test4.csv", header=None)
-z=df_[df_.columns[0:8]].values
+df=pd.read_csv("train.csv", header=None)
+x=df[df.columns[0:4]]
+y=df[df.columns[4:5]]
+df_=pd.read_csv("test.csv", header=None)
+z=df_[df_.columns[0:4]].values
 
 print y.size
 
